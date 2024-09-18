ef5d0f7b0b818f2de41a79b12a2b34f469e8c195
Phelix <phelixmbani@gmail.com>
2024-09-18 06:05:02 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+function writeCards(namesArray, eventName) {
+    let messages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+  
+      messages.push(message);
+    }
+  
+    
+    return messages;
+  }
+  
+
+  //console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+   function countDown(n) {
+    let i = 10;
+    while(i >= 0 ){
+      console.log(i);
+      i--;
+    }
+    return i;
+   }
+   countDown(10);
